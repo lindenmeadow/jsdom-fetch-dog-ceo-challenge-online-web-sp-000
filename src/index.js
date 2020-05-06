69bd034dcbad967cf7f09c47aab0d6b78b1f67a5
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-06 19:56:17 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-console.log('%c HI', 'color: firebrick')
+const IMG_URL = "https://dog.ceo/api/breeds/image/random/4"
+const BREED_URL = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener("DOMContentLoaded", initPage)
+
+function initPage (){
+  fetchDogs()
+  fetchBreeds()
+  addDropDownHander()
+}
+
+function fetchDogs(){
+  fetch(IMG_URL)
+  .then(function(response){return response.json()})
+  .then(function(myJson){myJson.message.forEach(renderDog)})
+}
+
+function fetchBreeds(){
+  fetch(BREED_URL)
+  .then(function(response){return response.json()})
+  .then(function(myJson){(Object.keys(myJson.message).forEach(renderBreed))})
+}
+
+function renderBreed(breed) {
+  let ul = document.querySelector("#dog-breeds")
+  let li = document.createElement('li');
+  li.innerHTML = breed
+  li.addEventListener('click', function() {
+    li.style.color = 'blue';
+  })
+  if (li.innerHTML[0] === document.querySelector('#breed-dropdown').value) {
+    ul.appendChild(li)
+  }
+}
+
+function renderDog(dog) {
+  var img = document.createElement('img');
+  img.src = dog
+  document.body.appendChild(img)
+}
+
+function addDropDownHander () {
+  let ul = document.querySelector("#dog-breeds")
+  drop = document.querySelector('#breed-dropdown')
+
+  drop.addEventListener('change', function () {
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+    fetchBreeds()
+  })
+}
