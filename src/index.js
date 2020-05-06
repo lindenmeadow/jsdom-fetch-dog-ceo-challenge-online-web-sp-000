c8e2d74c7220c932234e86c103751d3de78e09e4
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-05-06 19:55:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,20 +1,52 @@
-console.log('%c HI', 'color: firebrick')
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const IMG_URL = "https://dog.ceo/api/breeds/image/random/4"
+const BREED_URL = 'https://dog.ceo/api/breeds/list/all'
 
-  fetch(imgUrl)
-  .then(response => response.json())
-  .then(result => {
-    dogImages(result.message)
+document.addEventListener("DOMContentLoaded", initPage)
+
+function initPage (){
+  fetchDogs()
+  fetchBreeds()
+  addDropDownHander()
+}
+
+function fetchDogs(){
+  fetch(IMG_URL)
+  .then(function(response){return response.json()})
+  .then(function(myJson){myJson.message.forEach(renderDog)})
+}
+
+function fetchBreeds(){
+  fetch(BREED_URL)
+  .then(function(response){return response.json()})
+  .then(function(myJson){(Object.keys(myJson.message).forEach(renderBreed))})
+}
+
+function renderBreed(breed) {
+  let ul = document.querySelector("#dog-breeds")
+  let li = document.createElement('li');
+  li.innerHTML = breed
+  li.addEventListener('click', function() {
+    li.style.color = 'blue';
   })
+  if (li.innerHTML[0] === document.querySelector('#breed-dropdown').value) {
+    ul.appendChild(li)
+  }
+}
 
-  function dogImages(dogs){
-    console.log(dogs)
-    dogs.forEach(dog => {
-      console.log(dog)
-      const goodBoyContainer = document.querySelector('#dog-image-container')
-      const goodBoy = document.createElement('img')
-      goodBoy.src = dog
-      goodBoyContainer.appendChild(goodBoy)
-    });
-  } 
+function renderDog(dog) {
+  var img = document.createElement('img');
+  img.src = dog
+  document.body.appendChild(img)
+}
+
+function addDropDownHander () {
+  let ul = document.querySelector("#dog-breeds")
+  drop = document.querySelector('#breed-dropdown')
+
+  drop.addEventListener('change', function () {
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+    fetchBreeds()
+  })
+}
